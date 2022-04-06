f279a1203cda8fc312e8f31d7e7da4851c711d83
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-04-06 20:14:56 -0300

    Injeção de dependência

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -6,24 +6,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repositories.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
+	@Autowired
+	private PedidoRepository pedidoRepository;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		
-		Query query = entityManager.createQuery("SELECT p from Pedido p", Pedido.class);
-		
-		List<Pedido> pedidos = query.getResultList();
+		List<Pedido> pedidos = pedidoRepository.findPedidos();
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
