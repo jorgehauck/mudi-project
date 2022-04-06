4e5fe467fdab1225d7c84422e550db35e2a0d1fc
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-04-05 22:46:48 -0300

    Conexão persistindo dados

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +14,16 @@ import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51b0fG74oYL._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Redmi-branco-Vers%C3%A3o-Global/dp/B09D1SZZCD/ref=asc_df_B09D1SZZCD/?tag=googleshopp00-20&linkCode=df0&hvadid=379792730378&hvpos=&hvnetw=g&hvrand=542177759183153818&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001773&hvtargid=pla-1414472359641&psc=1");
-		pedido.setDescricao("Xiaomi de última geração TOP!");
 		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("SELECT p from Pedido p", Pedido.class);
+		
+		List<Pedido> pedidos = query.getResultList();
 		model.addAttribute("pedidos", pedidos);
 		return "home";
 	}
