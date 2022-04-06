5dfbfc0e4f5dbee3c9732ecc03d9aef71ce5362b
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-04-05 22:05:19 -0300

    Criação de conexão com banco de dados

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,8 +3,19 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataDaEntrega;
@@ -12,6 +23,24 @@ public class Pedido {
 	private String urlImagem;
 	private String descricao;
 	
+	public Pedido() {
+		
+	}
+	
+	public Pedido(String nomeProduto, BigDecimal valorNegociado, LocalDate dataDaEntrega, String urlProduto,
+			String urlImagem, String descricao) {
+		this.nomeProduto = nomeProduto;
+		this.valorNegociado = valorNegociado;
+		this.dataDaEntrega = dataDaEntrega;
+		this.urlProduto = urlProduto;
+		this.urlImagem = urlImagem;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
