e2a8bf338c760d03c27317ccb5bedbe0f8525aff
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2022-04-19 14:27:34 -0300

    Associando pedido ao usuário logado

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -3,6 +3,7 @@ package br.com.alura.mvc.mudi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.alura.mvc.mudi.dto.PedidoDTO;
 import br.com.alura.mvc.mudi.model.Pedido;
 import br.com.alura.mvc.mudi.model.StatusPedido;
+import br.com.alura.mvc.mudi.model.User;
 import br.com.alura.mvc.mudi.repositories.PedidoRepository;
+import br.com.alura.mvc.mudi.repositories.UserRepository;
 
 @Controller
 @RequestMapping("pedido")
@@ -20,6 +23,9 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("formulario")
 	public String formulario(PedidoDTO pedidoDTO) {
@@ -32,6 +38,13 @@ public class PedidoController {
 		if(result.hasErrors()) {
 			return "pedido/formulario";
 		}
+		/*
+		 * Variável username sendo atribuída a um método que retorna o nome do usuário
+		 * logado.
+		 */
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userRepository.findByUsername(username);
 		
 		Pedido pedido = new Pedido();
 		pedido.setNomeProduto(pedidoDTO.getNomeProduto());
@@ -39,6 +52,7 @@ public class PedidoController {
 		pedido.setUrlImagem(pedidoDTO.getUrlImagem());
 		pedido.setDescricao(pedidoDTO.getDescricao());
 		pedido.setStatus(StatusPedido.APROVADO);
+		pedido.setUser(user);
 		
 		pedidoRepository.save(pedido);
 		return "redirect:/home";
